509eeb6f706a469581dca1bf12e98ce9388e14dc
Dina Bellouki <dinab2002@gmail.com>
2025-05-26 15:42:37 +0200

    refactor

--- visualize_graph.js
+++ visualize_graph.js
@@ -61,32 +61,32 @@ async function render2DGraph(edgeFile, layoutFile, plotId) {
 }
 
 async function load3DGraphData(edgeFile, layoutFile) {
-      const [edgeData, nodeData] = await Promise.all([
+    const [edgeData, nodeData] = await Promise.all([
         d3.dsv(";", edgeFile),
         d3.dsv(";", layoutFile)
-      ]);
+    ]);
 
-      const edges = edgeData.map(d => [parseInt(d.from), parseInt(d.to)]);
-      const nodes = nodeData.map(d => ({
+    const edges = edgeData.map(d => [parseInt(d.from), parseInt(d.to)]);
+    const nodes = nodeData.map(d => ({
         x: parseFloat(d.x),
         y: parseFloat(d.y),
         z: parseFloat(d.z)
-      }));
+    }));
 
-      return { edges, nodes };
-    }
+    return { edges, nodes };
+}
 
-    function create3DTraces(edges, nodes) {
-      const edgeX = [], edgeY = [], edgeZ = [];
-      for (const [src, tgt] of edges) {
+function create3DTraces(edges, nodes) {
+    const edgeX = [], edgeY = [], edgeZ = [];
+    for (const [src, tgt] of edges) {
         const { x: x0, y: y0, z: z0 } = nodes[src];
         const { x: x1, y: y1, z: z1 } = nodes[tgt];
         edgeX.push(x0, x1, null);
         edgeY.push(y0, y1, null);
         edgeZ.push(z0, z1, null);
-      }
+    }
 
-      const edgeTrace = {
+    const edgeTrace = {
         x: edgeX,
         y: edgeY,
         z: edgeZ,
@@ -94,9 +94,9 @@ async function load3DGraphData(edgeFile, layoutFile) {
         type: 'scatter3d',
         line: { width: 1, color: '#888' },
         hoverinfo: 'none'
-      };
+    };
 
-      const nodeTrace = {
+    const nodeTrace = {
         x: nodes.map(n => n.x),
         y: nodes.map(n => n.y),
         z: nodes.map(n => n.z),
@@ -105,25 +105,25 @@ async function load3DGraphData(edgeFile, layoutFile) {
         marker: { size: 2, color: '#1f77b4' },
         text: nodes.map((_, i) => String(i)),
         hoverinfo: 'text'
-      };
+    };
 
-      return [edgeTrace, nodeTrace];
-    }
+    return [edgeTrace, nodeTrace];
+}
 
-    async function render3DGraph(edgeFile, layoutFile, plotId) {
-      const { edges, nodes } = await load3DGraphData(edgeFile, layoutFile);
-      const traces = create3DTraces(edges, nodes);
+async function render3DGraph(edgeFile, layoutFile, plotId) {
+    const { edges, nodes } = await load3DGraphData(edgeFile, layoutFile);
+    const traces = create3DTraces(edges, nodes);
 
-      const layout = {
+    const layout = {
         margin: { t: 20 },
         dragmode: 'orbit',
         scene: {
-          xaxis: { visible: false, showgrid: false, range: [0, 1] },
-          yaxis: { visible: false, showgrid: false, range: [0, 1] },
-          zaxis: { visible: false, showgrid: false, range: [0, 1] }
+            xaxis: { visible: false, showgrid: false, range: [0, 1] },
+            yaxis: { visible: false, showgrid: false, range: [0, 1] },
+            zaxis: { visible: false, showgrid: false, range: [0, 1] }
         },
         showlegend: false
-      };
+    };
 
-      Plotly.newPlot(plotId, traces, layout);
-    }
+    Plotly.newPlot(plotId, traces, layout);
+}
